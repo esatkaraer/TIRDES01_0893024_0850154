000e3be2693106c8c9b843dce5fca12a725348fc
Rhea <RheaHau@gmail.com>
2015-09-10 15:08:32 +0200

    Basic game.

--- HoneyShip/Game1.cs
+++ HoneyShip/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace HoneyShip
 {
@@ -15,9 +16,15 @@ namespace HoneyShip
             Content.RootDirectory = "Content";
         }
 
+        Vector2 shipPosition;
+        Texture2D shipAppearance;
+
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            shipPosition = new Vector2(400, 400);
+            shipAppearance = Content.Load<Texture2D>("ship.png");
 
             base.Initialize();
         }
@@ -26,6 +33,7 @@ namespace HoneyShip
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -37,10 +45,20 @@ namespace HoneyShip
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            //    Exit();
 
             // TODO: Add your update logic here
+            var keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.Escape))
+                Exit();
+            Func<bool, float> toUnit = b => b ? 1.0f : 0.0f;
+            var shipDelta = Vector2.Zero;
+            shipDelta += Vector2.UnitX * toUnit(keyboardState.IsKeyDown(Keys.Right));
+            shipDelta -= Vector2.UnitX * toUnit(keyboardState.IsKeyDown(Keys.Left));
+            shipDelta += Vector2.UnitY * toUnit(keyboardState.IsKeyDown(Keys.Down));
+            shipDelta -= Vector2.UnitY * toUnit(keyboardState.IsKeyDown(Keys.Up));
+            shipPosition += shipDelta * 5.0f;
 
             base.Update(gameTime);
         }
@@ -49,7 +67,9 @@ namespace HoneyShip
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(shipAppearance, shipPosition, Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
