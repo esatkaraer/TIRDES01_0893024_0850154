b85b5e6b370802b08aafae8f51ac82eea5424c07
esatkaraer <esatkaraer@gmail.com>
2015-09-15 11:51:18 +0200

    Added BgSong

--- HoneyShip/HoneyShipGame.cs
+++ HoneyShip/HoneyShipGame.cs
@@ -1,16 +1,29 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace HoneyShip
 {
     public class HoneyShipGame : Game
     {
+        [DllImport("winmm.dll")]
+        static extern Int32 mciSendString(string command, StringBuilder buffer, int bufferSize, IntPtr hwndCallback);
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         SpriteFont gameFont;
+
+        float deltaTime;
+        float oldGameTime;
+
         public HoneyShipGame()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -26,6 +39,8 @@ namespace HoneyShip
         Vector2 shipDelta;
         Texture2D bulletAppearance;
 
+        float friction = 0.1f;
+
         private float rotationAngle;
 
         List<Bullet> bullets = new List<Bullet>();
@@ -40,15 +55,19 @@ namespace HoneyShip
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             backgroundPosition = new Vector2(-150.0f,-150.0f);
+
             backgroundAppearance = Content.Load<Texture2D>("background.jpg");
 
             shipPosition = new Vector2(300.0f, 200.0f);
             shipAppearance = Content.Load<Texture2D>("ship.png");
-
+            
             bulletAppearance = Content.Load<Texture2D>("plasma.png");
 
-            gameFont = Content.Load<SpriteFont>("spaceFont");
+            mciSendString(@"open C:\Users\Esat\Desktop\Ascendency.wav type waveaudio alias Ascendency", null, 0, IntPtr.Zero);
+            mciSendString(@"play Ascendency", null, 0, IntPtr.Zero);
 
+   
+            gameFont = Content.Load<SpriteFont>("spaceFont");
         }
 
         protected override void UnloadContent()
@@ -60,6 +79,8 @@ namespace HoneyShip
         {
             KeyboardState ks = Keyboard.GetState();
             MouseState ms = Mouse.GetState();
+            oldGameTime = gameTime.TotalGameTime.Seconds;
+            deltaTime = oldGameTime - gameTime.TotalGameTime.Seconds;
 
             if (ks.IsKeyDown(Keys.Escape))
                 Exit();
@@ -82,12 +103,14 @@ namespace HoneyShip
             {
                 shipDelta += new Vector2(0.0f, 1.0f);
             }
-
+           
             shipPosition += shipDelta * 2.0f;
+
             if (!Window.ClientBounds.Contains(shipPosition))
             {
                 Exit();
             }
+
             Vector2 mouseLoc = new Vector2(ms.Position.X, ms.Position.Y);
             rotationAngle = MathHelper.ToDegrees((float)Math.Atan2(mouseLoc.Y - shipPosition.Y, mouseLoc.X - shipPosition.X));
 
@@ -110,6 +133,9 @@ namespace HoneyShip
                     bullet.direction = new Vector2((float)Math.Cos(MathHelper.ToRadians(rotationAngle)), (float)Math.Sin(MathHelper.ToRadians(rotationAngle))) * 4f;
                     bullet.isVisible = true;
                     bullets.Add(bullet);
+
+                    SoundPlayer gunEffect = new SoundPlayer(@"C:\Users\Esat\Desktop\science_fiction_laser_007.wav");
+                    gunEffect.Play();
                 }
                 bulletDelayer = 15;
             }
@@ -140,7 +166,7 @@ namespace HoneyShip
             spriteBatch.Begin();
             spriteBatch.Draw(backgroundAppearance, backgroundPosition, Color.White);
             spriteBatch.Draw(shipAppearance, shipPosition, null, Color.White, mousePositionAngle, origin, 1.0f, SpriteEffects.None, 0f);
-            spriteBatch.DrawString(gameFont, "Degrees : " + rotationAngle , new Vector2(10, 10), Color.White);
+            spriteBatch.DrawString(gameFont, "Elapsed Time : " + gameTime.TotalGameTime.Seconds , new Vector2(10, 10), Color.White);
             foreach(Bullet b in bullets)
             {
                 b.draw(spriteBatch);
