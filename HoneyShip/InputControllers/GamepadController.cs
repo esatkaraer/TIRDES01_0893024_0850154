961252d750c3d7b9aef4e9d76924d81835da2435
Esat Karaer <esatkaraer@outlook.com>
2015-10-29 02:15:03 +0100

    Added Joystick logic and some changes to sounds

--- HoneyShip/InputControllers/GamepadController.cs
+++ HoneyShip/InputControllers/GamepadController.cs
@@ -1,15 +1,59 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
+using SlimDX.DirectInput;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace HoneyShip
 {
-    /*class MainController : InputController
+    class GamepadController : InputController
     {
-        GamePadState gs;
+        Vector2 shipDelta;
+        Joystick joystick = null;
+        JoystickState js;
+        public GamepadController()
+        {
+            InitializeGamepad();
+        }
+
+        private void InitializeGamepad()
+        {
+            // make sure that DirectInput has been initialized
+            DirectInput dinput = new DirectInput();
+
+            // search for devices
+            foreach (DeviceInstance device in dinput.GetDevices(DeviceClass.GameController, DeviceEnumerationFlags.AttachedOnly))
+            {
+                // create the device
+                try
+                {
+                    joystick = new SlimDX.DirectInput.Joystick(dinput, device.InstanceGuid);
+                    break;
+                }
+                catch (DirectInputException)
+                {
+                }
+            }
+
+            if (joystick == null)
+            {
+                MessageBox.Show("There are no joysticks attached to the system.");
+                return;
+            }
+
+            foreach (DeviceObjectInstance deviceObject in joystick.GetObjects())
+            {
+                if ((deviceObject.ObjectType & ObjectDeviceType.Axis) != 0)
+                    joystick.GetObjectPropertiesById((int)deviceObject.ObjectType).SetRange(-1000, 1000);
+
+                //UpdateControl(deviceObject);
+            }
+
+            // acquire the device
+            joystick.Acquire();
+        }
 
         public bool Quit
         {
@@ -19,22 +63,44 @@ namespace HoneyShip
             }
         }
 
+
         public Vector2 ShipMovement(float rotationAngle)
         {
-            return new Vector2(0, 0);
+            shipDelta = Vector2.Zero;
+            if (js.X == 1000)
+            {
+                shipDelta += new Vector2((float)Math.Cos((rotationAngle) + MathHelper.PiOver2), (float)Math.Sin((rotationAngle + MathHelper.PiOver2)));
+            }
+            if (js.X == -1000)
+            {
+                shipDelta += new Vector2((float)Math.Cos((rotationAngle) - MathHelper.PiOver2), (float)Math.Sin((rotationAngle - MathHelper.PiOver2)));
+            }
+            if (js.Y == -1000)
+            {
+                shipDelta += new Vector2((float)Math.Cos((rotationAngle)), (float)Math.Sin((rotationAngle)));
+            }
+            if (js.Y == 1000)
+            {
+                shipDelta += new Vector2(-(float)Math.Cos((rotationAngle)), -(float)Math.Sin((rotationAngle)));
+            }
+            if (js.IsPressed(7))
+            {
+                shipDelta = shipDelta * 2f;
+            }
+            return shipDelta;
         }
 
         public bool isShooting
         {
             get
             {
-                return false;
+                return js.IsPressed(4);
             }
         }
 
         public void Update(float dt)
         {
-            gs = GamePad.GetState(PlayerIndex.One);
+            js = joystick.GetCurrentState();
         }
-    }*/
+    }
 }
